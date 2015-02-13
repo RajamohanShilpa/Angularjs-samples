25f49080340259afe2817ba84c2694cc1ed6125e
Rajamohan <raja444pln@gmail.com>
2015-02-13 19:09:01 +0530

    Create angularjs-project.js

---
+++ angularjs-project/angularjs-project.js
@@ -0,0 +1,6 @@
+$._addScript('/angularjs-project/configuration/configuration.js');
+$._addScript('/angularjs-project/main/main.js');
+$._addScript('/angularjs-project/sample-project/sample-project.js');
+
+
+$._addStyle('/angularjs-project/angularjs-project.css');
