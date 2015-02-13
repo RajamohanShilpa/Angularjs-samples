188c1cd8a5260ea74bc941dd9beca4243aa7a3c0
Rajamohan <raja444pln@gmail.com>
2015-02-13 19:25:44 +0530

    Create sample-project.js

---
+++ angularjs-project/sample-project/sample-project.js
@@ -0,0 +1,4 @@
+$._addScript('/angularjs-project/sample-project/sample-project-ctrl.js');
+$._addScript('/angularjs-project/sample-project/calendar/calendar.js');
+$._addScript('/angularjs-project/sample-project/ngAnimation/ngAnimation.js');
+$._addScript('/angularjs-project/sample-project/bootstrapConfirmation/bootstrapConfirmation.js');
